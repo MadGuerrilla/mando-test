ab88b6be70729ac402bcaddbf11317e4b29a7e0f
Krishan Veerappa <krishanveerappa@gmail.com>
2020-09-24 10:44:31 +0100

    ...

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,42 @@
       </div>
     </div>
   </div>
+  <div class="o-filters">
+    <button class="c-filters__button" @click="filter()">Show All</button>
+    <button class="c-filters__button" @click="filter('html')">HTML</button>
+    <button class="c-filters__button" @click="filter('javascript')">JavaScript</button>
+    <button class="c-filters__button" @click="filter('CSS')">CSS</button>
+  </div>
+  <div class="o-courses">
+    <div :id="course.courseId" v-for="course in courses" class="c-course">
+      <div :class="'c-course__icon c-course__icon--' + course.category.toLowerCase().trim()">
 
-  {{userData}}
+      </div>
+      <div class="c-course__description">
+        <h4>{{course.title}}</h4>
+        <p>
+          {{course.description}}
+        </p>
+      </div>
+      <div class="c-course__details">
+        <div class="c-course__details-more" @click="expand(course.courseId)">Course Details</div>
+        <ul :id="'c-course__details-list' + course.courseId" class="c-course__details-list">
+          <li class="c-course__details-list-item" v-for="detail in course.details">
+            {{detail}}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="o-form">
+    <form v-on:submit.prevent>
+      <label for="name">Name</label>
+      <input v-model="name" id="name" class="c-form__input" type="text" required />
+      <label for="bio">Bio</label>
+      <textarea v-model="bio" id="bio" class="c-form__text"></textarea>
+      <button class="c-form__button" @click="submit()">Submit</button>
+    </form>
+  </div>
 </div>
 </template>
 
@@ -42,6 +76,9 @@ export default {
   data() {
     return {
       userData: null,
+      courses: null,
+      name: null,
+      bio: null
     }
   },
   mounted() {
@@ -51,11 +88,15 @@ export default {
     getUserData: function() {
       axios
         .get('/static/data.json')
-        .then(response => this.userData = response.data)
+        .then((response) => {
+          console.log(response)
+          this.userData = response.data;
+          this.courses = response.data.courses;
+        })
     },
     modulesCalc: function(type) {
       let count = 0
-      this.userData.courses.filter((val) => {
+      this.courses.filter((val) => {
         if (val.status === type) count++
       })
       return count
@@ -64,6 +105,28 @@ export default {
       const courseCount = this.userData.courses.length
       const completed = this.modulesCalc('completed')
       return (completed / courseCount * 100)
+    },
+    filter: function(cat) {
+      this.courses = this.userData.courses
+      if (cat == null) {
+        return
+      } else {
+        this.courses = this.courses.filter((val) => {
+          if (val.category === cat) return val
+        })
+      }
+    },
+    expand: function(i) {
+      event.target.classList.toggle('c-course__details-more--active');
+      const list = document.getElementById('c-course__details-list' + i);
+      list.classList.toggle('c-course__details-list--show');
+    },
+    submit: function() {
+      if (this.name && this.bio) {
+        this.userData.name = this.name;
+        this.userData.username = this.name.split('').reverse().join('') + Math.floor(Math.random() * 90000) + 10000;
+        this.userData.bio = this.bio;
+      }
     }
   }
 }
@@ -71,13 +134,22 @@ export default {
 
 <style lang="scss" scoped>
 $green: #aadb1e;
+$darkGrey: #565656;
+$midGrey: #b7b7b7;
 $grey: #ccc;
+$lightGrey: #f4f4f4;
+$white: #fff;
+*,
+*:after,
+*:before {
+    box-sizing: border-box;
+}
 .l {
     &-default {
         font-family: Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        color: #565656;
+        color: $darkGrey;
         max-width: 1200px;
         width: 100%;
         margin: 24px auto;
@@ -93,6 +165,42 @@ $grey: #ccc;
         min-height: 196px;
         display: flex;
     }
+    &-courses {
+        margin: 16px auto;
+        display: grid;
+        grid-template-rows: auto;
+        grid-template-columns: repeat(1, 1fr);
+        @media (min-width: 768px) {
+            grid-template-columns: repeat(3, 1fr);
+        }
+        @media (min-width: 992px) {
+            grid-template-columns: repeat(4, 1fr);
+        }
+        .c-course:first-child {
+            margin-left: 0;
+        }
+        .c-course:last-child {
+            margin-right: 0;
+        }
+    }
+    &-filters {
+        margin: 24px 0;
+        display: flex;
+        align-items: center;
+        .c-filters__button:first-child {
+            margin-left: 0;
+        }
+    }
+    &-form {
+        width: 100%;
+        border-radius: 4px;
+        border: 1px solid $grey;
+        padding: 32px;
+        form {
+            display: flex;
+            flex-flow: column;
+        }
+    }
 }
 
 .c {
@@ -103,14 +211,14 @@ $grey: #ccc;
         }
         &__avatar {
             padding: 32px;
-            @media only screen and (min-width: 992px) {
+            @media (min-width: 768px) {
                 max-width: 10%;
                 background: url("./assets/svgs/avatar.svg") no-repeat top center;
                 background-size: contain;
             }
         }
         &__details {
-            @media only screen and (min-width: 992px) {
+            @media (min-width: 768px) {
                 max-width: 60%;
             }
             padding: 32px;
@@ -157,6 +265,117 @@ $grey: #ccc;
                     }
                 }
             }
+        }
+    }
+    &-course {
+        border-radius: 4px;
+        border: 1px solid $grey;
+        margin-left: 16px;
+        &__icon {
+            background-repeat: no-repeat;
+            background-size: contain;
+            background-position: center;
+            width: 100%;
+            height: 96px;
+            padding: 16px;
+            margin: 32px auto;
+            &--html {
+                background-image: url("./assets/svgs/html5.svg");
+            }
+            &--css {
+                background-image: url("./assets/svgs/css.svg");
+            }
+            &--javascript {
+                // background-image: url('./assets/svgs/javascript.svg');
+            }
+        }
+        &__description {
+            background-color: $lightGrey;
+            padding: 16px;
+            font-weight: bold;
+        }
+        &__details {
+            &-more {
+                background-color: $midGrey;
+                padding: 8px 16px;
+                font-weight: bold;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                cursor: pointer;
+                &:after {
+                    width: 16px;
+                    height: 16px;
+                    content: '';
+                    background-image: url("./assets/svgs/arrows.svg");
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                    background-position: center;
+                }
+                &--active {
+                    background-color: $darkGrey;
+                    color: $white;
+                    &:after {
+                      background-image: url("./assets/svgs/arrows-white.svg");
+                      transform: rotate(180deg);
+                    }
+                }
+            }
+            &-list {
+                // padding: 0;
+                // margin: 0;
+                height: 0;
+                overflow: hidden;
+                margin-top: 0;
+                margin-bottom: 0;
+
+                &-item {
+                    margin-bottom: 8px;
+                    font-weight: bold;
+                }
+                &--show {
+                    height: 100%;
+                    padding-top: 16px;
+                    padding-bottom: 16px;
+                }
+            }
+        }
+    }
+    &-filters {
+        &__button {
+            padding: 16px;
+            background-color: $green;
+            border-radius: 4px;
+            border: 0;
+            font-size: 1rem;
+            color: $darkGrey;
+            font-weight: bold;
+            min-width: 200px;
+            margin: 0 16px;
+        }
+    }
+    &-form {
+        &__input,
+        &__text {
+            width: 100%;
+            max-width: 400px;
+            height: 48px;
+            border-radius: 4px;
+            border: 1px solid $grey;
+            margin-bottom: 24px;
+        }
+        &__text {
+            height: 300px;
+        }
+        &__button {
+            padding: 16px;
+            background-color: $green;
+            border-radius: 4px;
+            border: 0;
+            font-size: 1rem;
+            color: $darkGrey;
+            font-weight: bold;
+            width: 200px;
         }
     }
 }
